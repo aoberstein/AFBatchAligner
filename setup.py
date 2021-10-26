9e4b0a369e7bb71edeae63def0dc03679cc640a4
Adam Oberstein <adam.oberstein@gmail.com>
2021-10-26 17:13:35 -0500

    2021-10-26-4 silver added setup.py to install v1.0.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AFBatchAligner',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='GPLv3',
+    author='Adam Oberstein',
+    author_email='adam.oberstein@gmail.com',
+    description='AlphaFold (AF) batch alignment tools'
+)
