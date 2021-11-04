5701adf1e573a997148831f05b6bca662ae4a7ce
Adam Oberstein <adam.oberstein@gmail.com>
2021-11-03 19:15:08 -0500

    2021-11-03 silver first commit v2.0.1 (cleaned)

--- src/14-lengthFilterclusters.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-from lengthFilterClusters import lengthFilter
-
-## format pdb database (exclude all non-AA residues and chains)
-clusterList = "/home/adam/archive/db/pdb/2021-10-26/pdb_clusters/bc-40.list"
-lengthToPdbTSV = "/home/adam/archive/db/pdb/2021-10-26/FATCATdb_2021-11-02/pdbLengths.tsv"
-lengthFilter( clusterList, lengthToPdbTSV, 80)
--- src/15-troubleshoot_7jsg_pdbFile.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-from fatcat_functions3 import formatPDB_db
-
-## format pdb database (exclude all non-AA residues and chains)
-dbRoot = "/home/adam/projects/2021-11-01_U94_analysis/db"
-formatPDB_db(dbLocation=dbRoot, cores=32)
